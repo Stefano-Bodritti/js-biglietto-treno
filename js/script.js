990b1b3124caedcdfe8e3a1282abfdf255b84ec8
Stefano <tepo@hotmail.it>
2021-02-26 15:54:56 +0100

    Calcolo prezzo ed eventuali sconti

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 //Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
 
 // 1. chiedo i km da percorrere
-var km = prompt("Quanti chilometri devi percorrere?")
+var km = prompt("Quanti chilometri devi percorrere?");
 
 // 2. chiedo l'età
-var eta = prompt("Quanti anni hai?")
+var eta = prompt("Quanti anni hai?");
 
 // 3. calcolo il costo
+var costo = km * 0.21;
 
 // 4. applico l'eventuale sconto
+if (eta >= 65) {
+  costo = costo / 100 * 60;
+} else if (eta < 18) {
+  costo = costo / 100 * 80;
+}
 
 // 5. stampo il risultato
