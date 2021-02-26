da4b0bc358ffe1dcdf0a80658d676f0da64d017d
Stefano <tepo@hotmail.it>
2021-02-26 15:28:43 +0100

    Richiesta età e km

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+// 1. chiedo i km da percorrere
+var km = prompt("Quanti chilometri devi percorrere?")
+
+// 2. chiedo l'età
+var eta = prompt("Quanti anni hai?")
+
+// 3. calcolo il costo
+
+// 4. applico l'eventuale sconto
+
+// 5. stampo il risultato
